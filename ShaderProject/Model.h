287a84e8e9cc37ab84c272af663e4efd24ee61f7
Andreas Pointner <pointner.andy@gmail.com>
2016-12-08 18:12:09 +0100

    extracted scene setup into seperate classes (wip)

--- ShaderProject/Model.h
+++ ShaderProject/Model.h
@@ -218,6 +218,8 @@ GLint TextureFromFile(const char* path, string directory, bool gamma)
 	glTexImage2D(GL_TEXTURE_2D, 0, gamma ? GL_SRGB : GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 
+
+
 	// Parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
